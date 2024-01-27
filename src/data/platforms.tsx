51ff29615be447d242966245bb8013f5172e18f1
Damilola Akinlade <damak.dev@gmail.com>
2024-01-27 17:08:17 +0100

    platforms

---
+++ src/data/platforms.tsx
@@ -0,0 +1,44 @@
+export const platforms = [
+  {
+    name: "Book Bazaar",
+    amount: 2500000,
+    rate: "+15%",
+    width: "55%",
+  },
+  {
+    name: "Artisan Aisle",
+    amount: 1800000,
+    rate: "+10%",
+    width: "43%",
+  },
+  {
+    name: "Toy Troop",
+    amount: 1200000,
+    rate: "+8%",
+    width: "31%",
+  },
+  {
+    name: "XStore",
+    amount: 900000,
+    rate: "+6%",
+    width: "25%",
+  },
+  {
+    name: "Artisan Aisle",
+    amount: 1800000,
+    rate: "+10%",
+    width: "55%",
+  },
+  {
+    name: "Toy Troop",
+    amount: 1200000,
+    rate: "+8%",
+    width: "55%",
+  },
+  {
+    name: "XStore",
+    amount: 900000,
+    rate: "+6%",
+    width: "25%",
+  },
+];
