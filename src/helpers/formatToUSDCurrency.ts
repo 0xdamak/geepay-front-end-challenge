8879ac83099372ba8e417d41f72e859cd11bcf3f
Damilola Akinlade <damak.dev@gmail.com>
2024-01-27 17:04:10 +0100

    formatToUSDCurrency

---
+++ src/helpers/formatToUSDCurrency.ts
@@ -0,0 +1,8 @@
+export function formatToUSDCurrency(number = 0): string {
+  return number.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+}
